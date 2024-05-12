8d7bb032978cce687bdac02139616e28afa8afb4
cintiacd_personal <cintia.301296@hotmail.com>
2024-05-12 20:40:25 -0300

    atualiza repo com modelo de face recognition

--- models/main.py
+++ models/main.py
@@ -4,38 +4,41 @@ import face_recognition
 # Inicializa a webcam
 video_capture = cv2.VideoCapture(0)
 
+# Verifica se a câmera foi aberta corretamente
+if not video_capture.isOpened():
+    print("Erro ao abrir a câmera.")
+    video_capture.release()
+    cv2.destroyAllWindows()
+    exit()
+
 # Loop principal
 while True:
     ret, frame = video_capture.read()
     if not ret:
+        print("Falha ao capturar imagem da câmera.")
         continue
 
-    # Converte a imagem de BGR para RGB (necessário para face_recognition)
+    # Converte a imagem capturada de BGR para RGB
     rgb_frame = frame[:, :, ::-1]
 
-    # Encontra todos os rostos no frame atual
+    # Encontra os locais dos rostos detectados na imagem
     face_locations = face_recognition.face_locations(rgb_frame)
 
-    # Verifica o número de rostos detectados
-    if len(face_locations) == 2:
-        # Desenha retângulos ao redor de cada rosto detectado
-        for (top, right, bottom, left) in face_locations:
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)  # Desenha um retângulo verde ao redor dos rostos
-    elif len(face_locations) > 2:
-        # Mostra uma mensagem de erro se mais de dois rostos forem detectados
-        cv2.putText(frame, "Mais de duas faces detectadas, por favor, se posicione em um ambiente mais tranquilo!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    elif len(face_locations) == 1:
-        # Mostra uma mensagem pedindo ao usuário para posicionar-se corretamente com o documento
-        cv2.putText(frame, "Por favor, posicione-se em frente a câmera e segure seu documento ao lado do rosto!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    else:
-        # Se nenhum rosto for detectado, talvez você queira adicionar uma mensagem aqui
-        pass
+    # Calcula os encodings dos rostos detectados
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    # Exibe o frame
-    cv2.imshow('Video', frame)
+    # Processa cada rosto detectado
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # Desenha um retângulo ao redor de cada rosto detectado
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+    # Exibe a imagem resultante
+    cv2.imshow('Webcam Face Detection', frame)
+
+    # Pressione 'q' para sair do loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Libera a câmera e fecha todas as janelas abertas
 video_capture.release()
 cv2.destroyAllWindows()
-
